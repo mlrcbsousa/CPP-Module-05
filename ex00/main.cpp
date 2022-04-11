b9221f483595ab5fe53a6d6bafc3cb1964d28c9c
Manuel Sousa <mlrcbsousa@gmail.com>
2022-04-11 18:01:29 +0200

    Add some tests in the main.cpp file.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:20:00 by msousa            #+#    #+#             */
-/*   Updated: 2022/04/10 21:09:53 by msousa           ###   ########.fr       */
+/*   Updated: 2022/04/11 18:01:04 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,34 +19,35 @@ int	main( void )
 	LOG(bob);
 
 	// test bad construction
-	// try {
-	// 	bob = Bureaucrat("bob", 0);
-	// }
-	// catch (std::exception & e) {
-	// 	/* handle exception */
-	// }
+	try {
+		bob = Bureaucrat("bob", 0);
+	}
+	catch (Bureaucrat::GradeTooHighException & e) {
+		ERROR(e.what());
+	}
 
-	// try {
-	// 	bob = Bureaucrat("bob", 151);
-	// }
-	// catch (std::exception & e) {
-	// 	/* handle exception */
-	// }
+	try {
+		bob = Bureaucrat("bob", 151);
+	}
+	catch (Bureaucrat::GradeTooLowException & e) {
+		ERROR(e.what());
+	}
 
 	// test out of bounds increment
-	// try {
-	// 	bob.incGrade();
-	// }
-	// catch (Bureaucrat::GradeTooHighException& e) {
-	// 	ERROR(e.what());
-	// }
+	try {
+		bob.incGrade();
+	}
+	catch (Bureaucrat::GradeTooHighException & e) {
+		ERROR(e.what());
+	}
 
 	bob = Bureaucrat("bob", 150);
+
 	// test out of bounds decrement
 	try {
 		bob.decGrade();
 	}
-	catch (std::exception & e) {
+	catch (Bureaucrat::GradeTooLowException & e) {
 		ERROR(e.what());
 	}
 
